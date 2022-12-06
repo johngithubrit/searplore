dcd7dc4ffc4d5263e1575403ae8f9003e877201c
johngithubrit <119733441+johngithubrit@users.noreply.github.com>
2022-12-06 18:31:30 +0530

    Update backend.js

--- backend.js
+++ backend.js
@@ -92,94 +92,6 @@ app.use(express.static("public"));
       axios.request(options).then(function(response) {
         var l = Object.entries(response.data.data);
         numLength= l.length;
-          console.log(numLength);
-         if(searchStr === "" || numLength === 0){
-          numLength = "";
-          heightSearch = 0;
-          widthSearch = 0;
-          widthPading = 0;
-          seaAbt="";
-          resAbt="";
-          heightBox = 0;
-        }else {
-        heightBox = "";
-        heightSearch = 5;
-        widthPading = 20;
-        heightBox=100;
-          widthSearch = "search-results";
-          seaAbt="About";
-          resAbt="search results";
-        }
-        for(var i=0; i<numLength;i++){
-          var origins = l[i][1].origin;
-        oriArr.push(origins);
-
-        var destinations = l[i][1].destination;
-        destArr.push(destinations);
-
-        var prices = l[i][1].price;
-        priceArr.push(prices);
-
-        var flightNum = l[i][1].flight_number;
-        flightnArr.push(flightNum);
-
-        var depart = l[i][1].departure_at;
-        var newDepart = depart.replace('T'," 𝗧𝗶𝗺𝗲:- ");
-
-        departArr.push(newDepart);
-        var returns = l[i][1].return_at;
-
-        var newreturns = returns.replace('T'," 𝗧𝗶𝗺𝗲:- ");
-        returnArr.push(newreturns);
-
-        var transfer = l[i][1].transfers;
-        transferArr.push(transfer);
-
-        airName = l[i][1].airline;
-        airnameArr.push(airName);
-
-        var numTrack = i;
-        ++numTrack;
-        numArr.push(numTrack);
-      }
-      if(req.isAuthenticated()){
-        res.render("main",{kindOfOri:oriArr,kindOfDest:destArr,kindOfPrice:priceArr,kindOfFlightN:flightnArr,kindOfDept:departArr
-          ,kindOflength:numLength,KindOfReturn:returnArr,kindOfTransfers:transferArr,kindOfAir:airnameArr,kindOfNum:numArr,kindOfHeight:heightSearch,
-        kindOfWidth:widthSearch,kindOfAbout:seaAbt,kindOfSearch:resAbt,kindOfBox:heightBox,kindOfpad:widthPading});
-      }else{
-         res.redirect("/search");
-      }
-    }).catch(function (error) {
-      res.render("main",{kindOfOri:oriArr,kindOfDest:destArr,kindOfPrice:priceArr,kindOfFlightN:flightnArr,kindOfDept:departArr
-        ,kindOflength:numLength,KindOfReturn:returnArr,kindOfTransfers:transferArr,kindOfAir:airnameArr,kindOfNum:numArr,kindOfHeight:heightSearch,
-      kindOfWidth:widthSearch,kindOfAbout:seaAbt,kindOfSearch:resAbt,kindOfBox:heightBox,kindOfpad:widthPading});
-});
-
-  });
-
-  app.get("/auth/google",
-  passport.authenticate('google', { scope: ['email','profile'] }));
-
-  app.get("/google/callback",
-    passport.authenticate('google', { failureRedirect: "/" }),
-    function(req, res) {
-      res.redirect("/search");
-    });
-
-    app.post("/search",function(req,res){
-        options = {
-        method: 'GET',
-        url: 'https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/city-directions',
-        params : {currency: 'USD',origin:searchStr},
-        headers: {
-          'X-Access-Token': '9ad7979ba03bd939e93bf75272fe5beb',
-          'X-RapidAPI-Key': '3491072d5fmsh3bf582674c53b81p1dd9d1jsndd4c1bc580a2',
-          'X-RapidAPI-Host': 'travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com'
-        }
-      };
-      axios.request(options).then(function(response) {
-        var l = Object.entries(response.data.data);
-        numLength= l.length;
          if(searchStr === "" || numLength === 0){
           numLength = "";
           heightSearch = 0;
@@ -309,4 +221,3 @@ app.use(express.static("public"));
 app.listen(PORT,function(){
   console.log("server has started successfully.");
 });
-
